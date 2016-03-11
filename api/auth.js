981626aa022b440c7460e68a2a65ed0f71e0e2aa
Kyle Nguyen <kyle@felizz.com>
2016-03-11 22:52:32 +0700

    Using Request

--- api/auth.js
+++ api/auth.js
@@ -6,22 +6,33 @@
  */
 
 var request = require('request');
-
+var expect = require('Chai').expect;
 
 describe("Authentication - Login", function () {
     var loginResponse;
 
-    it("should return Success if user exists", function () {
+    it("should return Success if user exists", function (done) {
         var postData = {
             username: "felizz",
             password: "123qwe"
         };
 
-        request.post({url:'http://dev.seanote.com:3000/v1/authentication/login', formData: postData}, function callback(err, httpResponse, body) {
+        request.post({
+            url:'http://dev.seanote.com:3000/v1/authentication/login',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            formData: {
+                username: "felizz",
+                password: "123qwe"
+            }
+        },
+            function callback(err, httpResponse, body) {
             if (err) {
                 return console.error('HTTP Call failed:', err);
             }
-            console.log('Upload successful!  Server responded with:', JSON.stringify(body));
+            console.log(' Server responded with:', JSON.stringify(body));
+            done();
         });
     });
 });
