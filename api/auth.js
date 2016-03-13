ca5a16c322133c7bbffb7ab8fea5b25ea7f381c3
Kyle Nguyen <kyle@felizz.com>
2016-03-13 22:52:58 +0700

    Inital Framework for API testing

--- api/auth.js
+++ api/auth.js
@@ -5,48 +5,19 @@
  * Created by kyle on 11/3/16.
  */
 
-var request = require('request');
 var expect = require('Chai').expect;
-var Ajv = require('ajv');
-var ajv = Ajv(); // options can be passed, e.g. {allErrors: true}
-
-var schema = {
-    "properties": {
-        "code": {
-            "type": "string"
-        },
-        "msg": {
-            "type": "object"
-        }
-    },
-    "required": [ "code", "msg"]
-};
+var testUtils = require('../utils');
 
 describe("Authentication - Login", function () {
-    var loginResponse;
 
     it("should return Success if user exists", function (done) {
-        var request = require("request");
-
-        var options = { method: 'POST',
-            url: 'http://dev.seanote.com:3000/v1/authentication/login',
-            headers:
-            {
-                'cache-control': 'no-cache',
-                'content-type': 'application/json' },
-            body: { username: 'felizz', password: '123qwe' },
-            json: true };
-
-        request(options, function (error, response, body) {
-            expect(error).to.not.exist;
-            var validResponse = ajv.validate(schema, body);
-            if(!validResponse){
-                console.log("Invalid Response: " + JSON.stringify(body));
-                throw new Error("Invalid Response");
+        testUtils.post('http://dev.seanote.com:3000/v1/authentication/login',  { username: 'felizz', password: '123qwe' },
+            function reqCallback(returnCode, data){
+                console.log(JSON.stringify(returnCode));
+                console.log(JSON.stringify(data));
+                expect(data.user_id).to.be.a('string');
+                done();
             }
-
-            done();
-        });
-
+        );
     });
 });
--- utils/index.js
+++ utils/index.js
@@ -3,13 +3,68 @@
  */
 
 var COOKIE  = null;
+var request = require("request");
+var Ajv = require('ajv');
+var ajv = Ajv(); // options can be passed, e.g. {allErrors: true}
+var expect = require('Chai').expect;
+
+
+var schema = {
+    "properties": {
+        "code": {
+            "type": "string"
+        },
+        "msg": {
+            "type": "object"
+        }
+    },
+    "required": [ "code", "msg"]
+};
+
 
 module.exports = {
-    expect: chakram.expect,
-    get : {
 
+    get: function (url, callback) {
+        var options = { method: 'GET',
+            url: url,
+            headers:
+            {
+                'cache-control': 'no-cache',
+                'content-type': 'application/json' },
+            json: true };
+
+        request(options, function (error, response, body) {
+            expect(error).to.not.exist;
+            var validResponse = ajv.validate(schema, body);
+            if(!validResponse){
+                console.log("Invalid Response: " + JSON.stringify(body));
+                throw new Error("Invalid Response");
+            }
+
+            callback(body.code, body.msg);
+        });
     },
-    login: function (username, password) {
 
+    post : function (url, reqBody, callback){
+
+        var options = { method: 'POST',
+            url: url,
+            headers:
+            {
+                'cache-control': 'no-cache',
+                'content-type': 'application/json' },
+            body: reqBody,
+            json: true };
+
+        request(options, function (error, response, body) {
+            expect(error).to.not.exist;
+            var validResponse = ajv.validate(schema, body);
+            if(!validResponse){
+                console.log("Invalid Response: " + JSON.stringify(body));
+                throw new Error("Invalid Response");
+            }
+
+            callback(body.code, body.msg);
+        });
     }
 };
