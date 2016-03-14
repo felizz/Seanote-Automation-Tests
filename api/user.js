50516b223a4f32c66091876e0233874bc42eebaa
Kyle Nguyen <kyle@felizz.com>
2016-03-14 11:17:37 +0700

    UpdateUserInfo Test

--- api/user.js
+++ api/user.js
@@ -95,3 +95,43 @@ describe("User - GetBasicInfo", function () {
     });
 
 });
+
+
+describe("User - UpdateUserInfo", function () {
+
+    it("should return basic info when user logged in ", function (done) {
+        testUtils.signupRandomUserThenLogIn(function loginCallback(user){
+
+            var reqData = {
+                first_name: "Testing",
+                gender: "m",
+                birthday: {
+                    day: 11,
+                    month: 11,
+                    year:1991
+                }
+            };
+
+            request.post('/v1/user/'+ user.id + '/info/update', reqData,
+                function reqCallback(code, data){
+                    expect(code).to.equal(returnCode.REQUEST_SUCCESS.code);
+                    logger.info(JSON.stringify(data));
+
+                    //Assess if user data is updated
+                    request.get('/v1/user/'+ user.id + '/info',
+                        function reqCallback(code, userData){
+                            expect(code).to.equal(returnCode.REQUEST_SUCCESS.code);
+                            logger.info(JSON.stringify(userData));
+                            expect(userData.first_name).to.equal(reqData.first_name);
+                            expect(userData.gender).to.equal(reqData.gender);
+                            expect(userData.birthday.day).to.equal(reqData.birthday.day);
+                            expect(userData.birthday.month).to.equal(reqData.birthday.month);
+                            expect(userData.birthday.year).to.equal(reqData.birthday.year);
+                            done();
+                        }
+                    );
+                }
+            );
+        });
+    });
+});
