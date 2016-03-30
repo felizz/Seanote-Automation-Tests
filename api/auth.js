6d9d81a9ec94c97f9f6c83c4ebacac06ff5f726d
Kyle Nguyen <kyle@felizz.com>
2016-03-30 16:50:50 +0700

    Fix Test & Support new style of errorFormat

--- api/auth.js
+++ api/auth.js
@@ -5,15 +5,15 @@
 var expect = require('chai').expect;
 var request = require('../lib/request');
 var logger = require('../lib/logger');
-var returnCode = require('../lib/return-code');
+var apiErrors = require('../lib/api-errors');
 
 describe("Authentication - Login", function () {
 
     it("should return Success if user exists", function (done) {
         request.clearCookie();
         request.post('/v1/authentication/login',  { username: 'felizz', password: '123qwe' },
-            function reqCallback(code, data){
-                expect(code).to.equal(returnCode.REQUEST_SUCCESS.code);
+            function reqCallback(err, data){
+                expect(err).to.be.a('null');
                 logger.info(JSON.stringify(data));
                 expect(data.user_id).to.be.a('string');
                 done();
@@ -24,9 +24,9 @@ describe("Authentication - Login", function () {
     it("should return failure if password is incorrect", function (done) {
         request.clearCookie();
         request.post('/v1/authentication/login',  { username: 'felizz', password: 'incorrect' },
-            function reqCallback(code, data){
-                expect(code).to.equal(returnCode.AUTHENTICATION_ERROR.code);
-                logger.info(JSON.stringify(data));
+            function reqCallback(err, data){
+                expect(err.error_name).to.equal(apiErrors.AUTHENTICATION_ERROR.new().error_name);
+                logger.info(JSON.stringify(err));
                 done();
             }
         );
@@ -35,9 +35,9 @@ describe("Authentication - Login", function () {
     it("should return failure if username does not exists", function (done) {
         request.clearCookie();
         request.post('/v1/authentication/login',  { username: 'felizz123', password: '123qwe' },
-            function reqCallback(code, data){
-                expect(code).to.equal(returnCode.DOES_NOT_EXIST.code);
-                logger.info(JSON.stringify(data));
+            function reqCallback(err, data){
+                expect(err.error_name).to.equal(apiErrors.AUTHENTICATION_ERROR.new().error_name);
+                logger.info(JSON.stringify(err));
                 done();
             }
         );
@@ -48,8 +48,9 @@ describe("Authentication - Logout", function () {
 
     it("should return Success ", function (done) {
         request.get('/v1/authentication/logout',
-            function reqCallback(code, data){
-                expect(code).to.equal(returnCode.REQUEST_SUCCESS.code);
+            function reqCallback(err, data){
+                expect(err).to.be.a('null');
+                expect(data).to.be.a('undefined');
                 logger.info(JSON.stringify(data));
                 done();
             }
